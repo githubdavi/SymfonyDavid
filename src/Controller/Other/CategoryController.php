8504135301b903e645afd77bbd7ab1c19e5987a5
david <david.valeriuss@gmail.com>
2024-11-26 11:00:18 +0100

    utilisation de la bdd pour les cat

--- src/Controller/Other/CategoryController.php
+++ src/Controller/Other/CategoryController.php
@@ -4,14 +4,26 @@ declare(strict_types=1);
 
 namespace App\Controller\Other;
 
+use App\Entity\Category;
+use App\Repository\CategoryRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Mapping\Entity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Attribute\Route;
 
 class CategoryController extends AbstractController
 {
     #[Route(path: '/categories', name: 'page_categories')]
-    public function categories()
+    public function categories(EntityManagerInterface $entityManager, CategoryRepository $categoryRepository)
     {
-        return $this->render('other/discover.html.twig');
+        $categories=$categoryRepository->findAll();
+        return $this->render('other/discover.html.twig',['categories' => $categories]);
+    }
+
+    #[Route(path: '/category/{id}', name: 'category')]
+    public function category(Category $category)
+    {
+        return $this->render('other/category.html.twig', ['categories' => $category]);
     }
 }
